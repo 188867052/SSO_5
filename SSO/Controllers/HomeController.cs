7fbf4c6fa54ddda3c1af66baf6086d72155b0167
召 程 <542153354@qq.com>
2020-12-13 20:35:29 +0800

    HomeController

--- SSO/Controllers/HomeController.cs
+++ SSO/Controllers/HomeController.cs
@@ -54,6 +54,26 @@ namespace SSO.Controllers
             return Redirect(url);
         }
 
+        private void Test1()
+        {
+            string url2 = $"{EDoc2_V5_Path}/api/services/Org/UserLoginIntegrationByUserLoginName";
+
+            string json = JsonConvert.SerializeObject(new
+            {
+                IntegrationKey = IntegrationKey,
+                LoginName = "EdocAdmin",
+                IPAddress = GetLocalIp(),
+            });
+
+            using var httpclientHandler = new HttpClientHandler();
+            httpclientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
+            using HttpClient httpClient = new HttpClient(httpclientHandler);
+            httpClient.BaseAddress = new Uri(EDoc2_V5_Path);
+            using StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var result = httpClient.PostAsync("/api/services/Org/UserLoginIntegrationByUserLoginName", stringContent).Result.Content.ReadAsStringAsync().Result;
+            _logger.LogInformation($" Test result: {result}");
+        }
+
         [HttpGet]
         [Route(SignInMicrosoftRoute)]
         public IActionResult SignInCallBack(string code)
@@ -61,6 +81,8 @@ namespace SSO.Controllers
             _logger.LogInformation($"SignInCallBack code: {code}");
             try
             {
+                Test1();
+
                 var keyValuePairs = new Dictionary<string, string>()
                 {
                     { "client_id", ClientId },
@@ -181,56 +203,6 @@ namespace SSO.Controllers
                 }
             }
             return AddressIP;
-        }
-
-        private static string PostUrl(string url, string postData)
-        {
-            HttpWebRequest request = null;
-            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
-            {
-                request = WebRequest.Create(url) as HttpWebRequest;
-                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
-                request.ProtocolVersion = HttpVersion.Version11;
-                // 这里设置了协议类型。
-                request.Timeout = 100000;
-                request.ReadWriteTimeout = 300000;
-
-                ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;// SecurityProtocolType.Tls1.2; 
-                request.KeepAlive = false;
-                ServicePointManager.CheckCertificateRevocationList = true;
-                ServicePointManager.DefaultConnectionLimit = 100;
-                ServicePointManager.Expect100Continue = false;
-            }
-            else
-            {
-                request = (HttpWebRequest)WebRequest.Create(url);
-            }
-
-            request.Method = "POST";    //使用get方式发送数据
-            request.ContentType = "application/json";
-            request.Referer = null;
-            request.AllowAutoRedirect = true;
-            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
-            request.Accept = "*/*";
-
-            byte[] data = Encoding.UTF8.GetBytes(postData);
-            using (Stream newStream = request.GetRequestStream())
-            {
-                newStream.Write(data, 0, data.Length);
-                newStream.Close();
-            }
-
-            //获取网页响应结果
-            using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            using Stream stream = response.GetResponseStream();
-            //client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-            using StreamReader sr = new StreamReader(stream);
-            return sr.ReadToEnd();
-        }
-
-        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
-        {
-            return true; //总是接受  
         }
     }
 }
